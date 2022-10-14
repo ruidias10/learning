fd02c77b4c6a8d4da97aaf4fd7989ca7604c5610
Rui Dias <rui.dias10@gmail.com>
2022-10-14 23:06:05 +0100

    Types CPP

--- cpp/lessons/tutorial-01/lesson-01.cpp
+++ cpp/lessons/tutorial-01/lesson-01.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Olá Mundo!\n";
+  std::cout << "Hell World!\n";
+
+  std::cout << 4 + 2 << "\n";
+  std::cout << 4 - 2 << "\n";
+  std::cout << 4 * 2 << "\n";
+  
+  std::cout << 4 / 2 << "\n";
+  std::cout << 5 / 2 << "\n";
+  std::cout << 5 % 2 << "\n";
+ 
+  int soma = 2 + 2;
+  soma = soma + 42;
+  std::cout << soma << "\n";
+
+  int i = 1;
+  double d = 1.2;
+  float f = 1.2; 
+  //std::string s = "String";
+  char c = 'C';
+  bool b = true;
 
   return 0;
-}
+} 
